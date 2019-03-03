82f9fa51687a37e8aa44f8ca912836976830a0f5
Jen Pirrone <pirronej104@gmail.com>
2019-03-03 16:03:02 -0500

    setting up git ignore file, config.js file, .env, and nodemodules

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: "root",
+    // make sure to set this env variable in your .env file
+    password: process.env.LOCAL_MYSQL_PASSWORD,
+    database: "PhillyMeIn",
+    host: "localhost",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "testdb",
+    host: "localhost",
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    /* eslint-disable-next-line camelcase */
+    use_env_variable: JAWSDB_URL,
+    dialect: mysql
+  }
+};
