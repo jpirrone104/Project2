9b72c7127fb81f94930305c4168d88a7de47e64d
Jen Pirrone <pirronej104@gmail.com>
2019-03-04 18:43:26 -0500

    fixing stupid mistakes

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   production: {
     /* eslint-disable-next-line camelcase */
-    use_env_variable: JAWSDB_URL,
-    dialect: mysql
+    use_env_variable: process.env.JAWSDB_URL,
+    dialect: "mysql"
   }
 };
