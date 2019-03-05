c537a8a93be8d24abb6e53596155e6627e25b542
Jen Pirrone <pirronej104@gmail.com>
2019-03-04 21:08:50 -0500

    Tour seeds

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO tour (title, description, neighborhood, numberOfStops, URL, duration, tags, createdAt, updatedAt) 
+VALUES 
+-- ("Fishtown Coffee Tour", "Fishtown is home to multiple roasters and unique coffee shops, sporting local fare and eclectic atmospheres.", "Fishtown", "4", "https://www.wheretraveler.com/sites/default/files/images/Philly_LaColombe-Fishtown2_0.jpg", "60", "Food, Culture, Art", now(), now()), 
+("East Passyunk Crafted Philly Tour", "From the crochet bollard covers to Isaiah Zagar's eclectic mixed-media murals, South Philly is home to some of the best street art and made-in-philly flair.", "East Passyunk", "6", "http://www.flyingkitemedia.com/galleries/Features/2013/Issue_123/passyunk(fountain).jpg", "120", "Art, Culture", now(), now()), 
+("Old City Historical Houses Tour", "Visit the homes of celebrated founding fathers, like William Penn and Benjamin Franklin, along with the Betsy Ross house and Edgar Allen Poe historical site. This tour will delight the young and the young at heart as you walk through the historical wonders of our nations first capitol.", "Old City", "9", "http://pabook2.libraries.psu.edu/palitmap/ElfrethPanorama2.jpg", "120", "History, Culture", now(), now())
+
+
+
+
+
+
