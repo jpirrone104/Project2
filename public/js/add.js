bcf7222fcdff2b3bca634daa9272d85007647bef
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-03-08 08:18:01 -0500

    Fix add button functionality

--- public/js/add.js
+++ public/js/add.js
@@ -27,13 +27,13 @@ var API = {
 // If the user wants to add another stop to the route, generate a blank input field.
 $(document).on("click", "#add-stop-button", function() {
   var submitButton = $("#submit-button");
-  var addStop = $(".tour-stop:last");
+  var addStop = $(".tourstop:last");
 
   // Clone the new stop input and clear contents of previous entry.
   var newStop = addStop.clone();
-  newStop.children(".stop-title").val("");
-  newStop.children(".stop-address").val("");
-  newStop.children(".stop-description").val("");
+  newStop.children(".stoptitle").val("");
+  newStop.children(".stopaddress").val("");
+  newStop.children(".stopdescription").val("");
 
   // Insert new stop before the submit button, then destroy the previous button.
   newStop.insertBefore(submitButton);
