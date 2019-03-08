270e9fbca7046a32c1487def4069537bc7d4ab74
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-03-07 21:33:04 -0500

    Add sample API routing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,24 @@
-// var db = require("../models");
+var db = require("../models");
 
-// module.exports = function(app) {
-//   // Get all examples
-//   app.get("/api/examples", function(req, res) {
-//     db.Example.findAll({}).then(function(dbExamples) {
-//       res.json(dbExamples);
-//     });
-//   });
+module.exports = function(app) {
+  // Get all examples
+  app.get("/api/tours", function(req, res) {
+    db.Tour.findAll({}).then(function(tours) {
+      res.json(tours);
+    });
+  });
 
-//   // Create a new example
-//   app.post("/api/examples", function(req, res) {
-//     db.Example.create(req.body).then(function(dbExample) {
-//       res.json(dbExample);
-//     });
-//   });
+  // Create a new example
+  app.post("/api/tours", function(req, res) {
+    db.Tour.create(req.body).then(function(tour) {
+      res.json(tour);
+    });
+  });
 
-//   // Delete an example by id
-//   app.delete("/api/examples/:id", function(req, res) {
-//     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-//       res.json(dbExample);
-//     });
-//   });
-// };
+  // Delete an example by id
+  //   app.delete("/api/examples/:id", function(req, res) {
+  //     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //       res.json(dbExample);
+  //     });
+  //   });
+};
