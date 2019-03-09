c8c111efb76cafbbf914294a2c9b50b92e464efb
Jen Pirrone <pirronej104@gmail.com>
2019-03-09 06:43:39 -0500

    updating models

--- models/tour.js
+++ models/tour.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     },
     URL: {
-      type: DataTypes.TEXT,
+      type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1000]
