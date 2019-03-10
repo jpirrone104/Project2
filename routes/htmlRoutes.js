029d0b530b82dc2b7d394d7ac433474b96ffd74d
Jen Pirrone <pirronej104@gmail.com>
2019-03-10 19:23:56 -0400

    merging passport and fixing bugs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   // Load index page
@@ -15,7 +16,7 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/add", function(req, res) {
+  app.get("/add", isAuthenticated, function(req, res) {
     res.render("add");
   });
 
@@ -66,23 +67,13 @@ module.exports = function(app) {
     });
   });
 
-  //   app.get("/", function(req, res) {
-  //     db.Example.findAll({}).then(function(dbExamples) {
-  //       res.render("index", {
-  //         msg: "Welcome!",
-  //         examples: dbExamples
-  //       });
-  //     });
-  //   });
+  app.get("/signup", function(req, res) {
+    res.render("signup");
+  });
 
-  // Load example page and pass in an example by id
-  //   app.get("/example/:id", function(req, res) {
-  //     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  //       res.render("example", {
-  //         example: dbExample
-  //       });
-  //     });
-  //   });
+  app.get("/login", function(req, res) {
+    res.render("login");
+  });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
